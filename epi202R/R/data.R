54ba949e6d9a3fbacae221cc0958a45b28d70c56
Nishant Kishore <nish.kishore@gmail.com>
2019-10-29 16:42:27 -0400

    added data

---
+++ epi202R/R/data.R
@@ -0,0 +1,16 @@
+#' Example Epidemiologic Data for Epi 202.
+#'
+#' Data from cohort study in which white males in Evans County were followed for 7 years, 
+#' with coronary heart disease as the outcome of interest.
+#'
+#' @format A data frame with 609 rows and 5 variables:
+#' \describe{
+#'   \item{CHD}{Coronary heart disease status}
+#'   \item{HTN}{Hypertension status}
+#'   \item{SMK}{Smoking status}
+#'   \item{person_time}{Amount of person time contributed in years}
+#'   \item{caco}{If data was part of a case control study}
+#'   ...
+#' }
+#' @source \url{http://web1.sph.emory.edu/dkleinb/logreg3.htm#data}
+"evansData"
